93c64511dcbc257cf7f39b7c5a68378b0198d21e
ExanVUB <127668600+ExanVUB@users.noreply.github.com>
2023-03-12 16:09:10 +0100

    Update 3D-plots.py

---
+++ 3D-plots.py
@@ -0,0 +1,21 @@
+#%matplotlib notebook
+#%matplotlib ipympl
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import os
+#os.chdir('/Users/nicolasvv/OneDrive - Vrije Universiteit Brussel/Onderzoek/n occipitalis tertius/microscribe n occ tertius')
+os.chdir('/Users/nicolas/Library/CloudStorage/OneDrive-VrijeUniversiteitBrussel/Onderzoek/n occipitalis tertius/microscribe n occ tertius')
+
+data = pd.read_excel('NOT 62_python - 1 side.xlsx')
+data
+
+fig = plt.figure(figsize=plt.figaspect(0.5))
+ax = fig.add_subplot(1,1,1, projection='3d')
+for i in np.unique(data['color']):
+    ax.plot(data['x1'][data['color'] == i],
+            data['y1'][data['color'] == i],
+            data['z1'][data['color'] == i], '-o')
+
+ax.set_xlabel('X'); ax.set_ylabel('Y'); ax.set_zlabel('Z')
